dcc73fa46f1a274e90760336a9ec063e0624f9bd
Sam Belliveau <sam.belliveau@gmail.com>
2024-11-17 15:15:52 -0500

    Fixed Bug with CMD+W & Added Graphing

--- src/index.js
+++ src/index.js
@@ -9,162 +9,196 @@ const {
   nativeImage,
 } = require("electron");
 const path = require("path");
-
 const settings = require("electron-settings");
 
-let showHideKey;
-
-let mainWindow;
-let tray;
-
-// Attempt to acquire the single instance lock
-const gotTheLock = app.requestSingleInstanceLock();
-
-if (!gotTheLock) {
-  // If another instance is running, quit this one
+app.disableHardwareAcceleration();
+if (!app.requestSingleInstanceLock()) {
   app.quit();
-} else {
-  app.disableHardwareAcceleration();
+}
 
-  // This is the primary instance
-  app.on("second-instance", (event, argv, workingDirectory) => {
-    // Someone tried to run a second instance, focus the window if necessary
-    if (mainWindow) {
-      if (mainWindow.isMinimized()) mainWindow.restore();
-      if (!mainWindow.isVisible()) mainWindow.show();
-      mainWindow.focus();
-    }
-  });
+let showHideKey = "CommandOrControl+Shift+D";
 
-  function createWindow() {
-    // Create the browser window but don't show it immediately
-    mainWindow = new BrowserWindow({
-      width: 600,
-      height: 500,
-      show: false,
-      frame: false,
-      skipTaskbar: true,
-      resizable: false,
-      alwaysOnTop: true,
-      fullscreenable: false,
-      webPreferences: {
-        backgroundThrottling: true,
-      },
-    });
+function getSetting(key, defaultValue) {
+  if (!settings.hasSync(key)) {
+    settings.setSync(key, defaultValue);
+    return defaultValue;
+  }
 
-    showHideKey = "Command+Shift+D";
+  return settings.getSync(key);
+}
 
-    mainWindow.loadFile("src/index.html");
-    mainWindow.setVisibleOnAllWorkspaces(true, { visibleOnFullScreen: true });
-    mainWindow.setAlwaysOnTop(true, "floating", 1);
-    mainWindow.webContents.setFrameRate(30);
+function setSetting(key, value) {
+  settings.setSync(key, value);
+}
 
-    if (!settings.hasSync("hideOnFocusLoss")) {
-      settings.setSync("hideOnFocusLoss", true);
-    }
+class MainApp {
+  constructor() {
+    this._mainWindow = null;
+    this.tray = null;
 
-    mainWindow.on("blur", () => {
-      if (settings.getSync("hideOnFocusLoss")) {
-        hideWindow();
+    app.on("second-instance", () => this.focusMainWindow());
+
+    app.on("will-quit", () => globalShortcut.unregisterAll());
+    app.on("window-all-closed", (e) => e.preventDefault());
+
+    app.on("close", () => (this._mainWindow = null));
+
+    app.whenReady().then(() => {
+      if (app.dock && app.dock.hide) {
+        app.dock.hide();
       }
+
+      this.mainWindow;
+      this.createTray();
+      this.registerShortcuts();
     });
   }
 
-  function toggleWindow() {
-    if (mainWindow.isVisible()) {
-      hideWindow();
-    } else {
-      showWindow();
+  get mainWindow() {
+    // Check for situations where the window is destroyed but not null
+    if (this._mainWindow) {
+      if (this._mainWindow.isDestroyed()) {
+        this._mainWindow = null;
+      }
     }
+
+    // If the window is null, create a new one
+    if (!this._mainWindow) {
+      let graphing = getSetting("useGraphingCalculator", false);
+
+      this._mainWindow = new BrowserWindow({
+        width: graphing ? 800 : 600,
+        height: 500,
+        show: false,
+        frame: false,
+        skipTaskbar: true,
+        resizable: false,
+        alwaysOnTop: true,
+        fullscreenable: false,
+        webPreferences: {
+          backgroundThrottling: true,
+        },
+      });
+
+      if (graphing) {
+        this._mainWindow.loadFile("src/graphing-calculator.html");
+      } else {
+        this._mainWindow.loadFile("src/scientific-calculator.html");
+      }
+
+      this._mainWindow.setVisibleOnAllWorkspaces(true, {
+        visibleOnFullScreen: true,
+      });
+      this._mainWindow.setAlwaysOnTop(true, "floating", 1);
+      this._mainWindow.webContents.setFrameRate(20);
+
+      this._mainWindow.on("blur", () => {
+        if (getSetting("hideOnFocusLoss", false)) {
+          this.hideWindow();
+        }
+      });
+    }
+
+    return this._mainWindow;
   }
 
-  function hideWindow() {
-    mainWindow.hide();
-  }
-
-  function showWindow() {
-    // Position the window near the tray icon
-    const trayBounds = tray.getBounds();
-    const windowBounds = mainWindow.getBounds();
-
-    // Calculate the position (for simplicity, we'll center it horizontally)
-    const x = Math.round(
-      trayBounds.x + trayBounds.width / 2 - windowBounds.width / 2
+  createTray() {
+    this.tray = new Tray(
+      nativeImage.createFromPath(
+        path.join(__dirname, "desmos", "iconTemplate@4x.png")
+      )
     );
-    const y = trayBounds.y + trayBounds.height + 4;
-
-    mainWindow.setPosition(x, y, false);
-    mainWindow.show();
-    mainWindow.focus();
-  }
-
-  function createTray() {
-    icon = nativeImage.createFromPath(
-      path.join(__dirname, "desmos", "iconTemplate@4x.png")
-    );
-    tray = new Tray(icon);
 
     const contextMenu = Menu.buildFromTemplate([
       {
         label: "Show/Hide",
         accelerator: showHideKey,
-        click: () => {
-          toggleWindow();
-        },
+        click: () => this.toggleWindow(),
       },
       {
-        label: "Hide on Focus Loss",
-        type: "checkbox",
-        checked: settings.getSync("hideOnFocusLoss"),
-        click: (menuItem) => {
-          settings.setSync("hideOnFocusLoss", menuItem.checked);
-        },
-      },
-      {
-        label: "Start at Login",
-        type: "checkbox",
-        checked: app.getLoginItemSettings().openAtLogin,
-        click: (menuItem) => {
-          app.setLoginItemSettings({
-            openAtLogin: menuItem.checked,
-            path: app.getPath("exe"),
-            args: [],
-          });
-        },
+        label: "Settings",
+        submenu: [
+          {
+            label: "Use Graphing Calculator",
+            type: "checkbox",
+            checked: getSetting("useGraphingCalculator", false),
+            click: (menuItem) => {
+              setSetting("useGraphingCalculator", menuItem.checked);
+              this.reloadWindow();
+            },
+          },
+          {
+            label: "Hide on Focus Loss",
+            type: "checkbox",
+            checked: getSetting("hideOnFocusLoss", false),
+            click: (menuItem) =>
+              setSetting("hideOnFocusLoss", menuItem.checked),
+          },
+          {
+            label: "Start at Login",
+            type: "checkbox",
+            checked: app.getLoginItemSettings().openAtLogin,
+            click: (menuItem) =>
+              app.setLoginItemSettings({
+                openAtLogin: menuItem.checked,
+                path: app.getPath("exe"),
+                args: [],
+              }),
+          }
+        ]
       },
       {
         label: "Quit",
-        click: () => {
-          app.quit();
-        },
+        click: () => app.quit(),
       },
     ]);
 
-    tray.setContextMenu(contextMenu);
+    this.tray.setContextMenu(contextMenu);
   }
 
-  app.whenReady().then(() => {
-    if (app.dock && app.dock.hide) {
-      app.dock.hide();
+  registerShortcuts() {
+    globalShortcut.register(showHideKey, () => this.toggleWindow());
+    globalShortcut.register("Escape", () => this.hideWindow());
+  }
+
+  toggleWindow() {
+    if (this.mainWindow.isVisible()) {
+      this.hideWindow();
+    } else {
+      this.showWindow();
     }
+  }
 
-    createWindow();
-    createTray();
+  hideWindow() {
+    this.mainWindow.hide();
+  }
 
-    globalShortcut.register(showHideKey, () => {
-      toggleWindow();
-    });
+  showWindow() {
+    const trayBounds = this.tray.getBounds();
+    const windowBounds = this.mainWindow.getBounds();
 
-    globalShortcut.register("Escape", () => {
-      hideWindow();
-    });
-  });
+    const x = Math.round(
+      trayBounds.x + trayBounds.width / 2 - windowBounds.width / 2
+    );
+    const y = trayBounds.y + trayBounds.height + 4;
 
-  app.on("will-quit", () => {
-    globalShortcut.unregisterAll();
-  });
+    this.mainWindow.setPosition(x, y, false);
+    this.mainWindow.show();
+    this.mainWindow.focus();
+  }
 
-  app.on("window-all-closed", (e) => {
-    e.preventDefault();
-  });
+  focus() {
+    if (this.mainWindow.isMinimized()) this.mainWindow.restore();
+    if (!this.mainWindow.isVisible()) this.mainWindow.show();
+    this.mainWindow.focus();
+  }
+
+  reloadWindow() {
+    this.hideWindow();
+    this.mainWindow.destroy();
+    this._mainWindow = null;
+    this.mainWindow;
+  }
 }
+
+new MainApp();
